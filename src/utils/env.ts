01cc840739f8be038ea476f0cbe7aca1c064f86c
zhaoyan <zhaoyan@tj-it.com.cn>
2024-11-15 17:24:57 +0800

    feat: 修改环境变量配置

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -20,7 +20,9 @@ export function getAppEnvConfig() {
   const ENV = (import.meta.env.DEV
     ? // Get the global configuration (the configuration will be extracted independently when packaging)
       (import.meta.env as unknown as GlobEnvConfig)
-    : window[ENV_NAME as any]) as unknown as GlobEnvConfig;
+    : window[ENV_NAME as any]
+    ? window[ENV_NAME as any]
+    : (import.meta.env as unknown as GlobEnvConfig)) as unknown as GlobEnvConfig;
 
   const {
     VITE_GLOB_APP_TITLE,
